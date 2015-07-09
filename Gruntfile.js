f19042217c68363f42a190f74e2d78242fa6cc20
vdumontier <vdumontier@gmail.com>
2015-07-09 14:52:57 +0200

    init

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    nodewebkit: {
+      options: {
+        appName: "nw-test",
+        platforms: ['win'],
+        buildDir: './dist',
+        version: "0.12.1"
+      },
+      src: ['./public/**/*']
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-node-webkit-builder');
+
+  grunt.registerTask('default', ['nodewebkit']);
+};
